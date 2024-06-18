d0048083dd14bea05d7114d393973adc23e618fe
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2024-06-18 12:14:49 +0100

    adjusted screen sizes to (temporary) ones that are useful for debugging

--- src/tetris/src/raylib_main.c
+++ src/tetris/src/raylib_main.c
@@ -27,8 +27,8 @@ Sound fxCoin = { 0 };
 //----------------------------------------------------------------------------------
 // Local Variables Definition (local to this module)
 //----------------------------------------------------------------------------------
-static const int screenWidth = 800;
-static const int screenHeight = 450;
+static const int screenWidth = 600; // TODO: was 800, adjust to final on release
+static const int screenHeight = 800; // TODO: was 450, adjust to final on release
 
 // Required variables to manage screen transitions (fade-in, fade-out)
 static float transAlpha = 0.0f;
