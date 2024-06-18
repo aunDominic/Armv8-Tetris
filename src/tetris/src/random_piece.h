e3f6bdd8e2149a32cb94f025f057df38b5c2d69c
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2024-06-18 15:36:49 +0100

    moved around a few definitions to support the idea that `random_piece.h` is an abstraction/strategy over actual randomness implementations

--- src/tetris/src/random_piece.h
+++ src/tetris/src/random_piece.h
@@ -1,8 +1,43 @@
 #ifndef RANDOM_PIECE_H
 #define RANDOM_PIECE_H
 
+#include "types.h"
 #include "tetriminoes.h"
 
-TetrominoType generate_piece(); // returns an int from 1-7
+typedef union {
+    u16_t integerEncoding: 15;
+    struct {
+        TetrominoType first: 3;
+        TetrominoType second: 3;
+        TetrominoType third: 3;
+        TetrominoType fourth: 3;
+        TetrominoType fifth: 3;
+    } pieces;
+} FivePiecesPreview;
+
+/// Returns the next five pieces that will be dropped,
+/// encoded as a 16-bit unsigned integer.
+///
+/// The bits mean the following:
+/// - 0-2: the first tetromino type, in the range of 0..7
+/// - 3-5: the second tetromino type, in the range of 0..7
+/// - 6-8: the third tetromino type, in the range of 0..7
+/// - 9-11: the fourth tetromino type, in the range of 0..7
+/// - 12-14: the fifth tetromino type, in the range of 0..7
+/// - 15: unused
+///
+/// A piece type of 0 is an empty piece, while 1..7 are the usual O,I,S,Z,L,J,T
+///
+/// They can be conveniently decoded by instantiating the `FivePiecesPreview` union
+/// with the returned integer, and then accessing the `.pieces` property
+u16_t viewNextFive();
+
+/// Will print the next five pieces to output, encoded as integer
+void printNextFive(u16_t nextFive);
+
+/// Function to generate the next piece,
+/// which returns an integer in the range of 0..7
+/// representing the tetromino type
+TetrominoType generate_piece();
 
 #endif //RANDOM_PIECE_H
--- src/tetris/src/seven_bag.c
+++ src/tetris/src/seven_bag.c
@@ -17,11 +17,11 @@
 static struct {
     size_t cursor;
     TetrominoType pieces[BAG_SIZE];
-} actualBag = { .cursor = BAG_SIZE, };
+} actualBag = { .cursor = BAG_SIZE };
 
 /// this represents the next seven pieces that were (or will be)
 /// produced by the actual bag - it is guaranteed to not diverge
-static TetrominoType facadeBag[BAG_SIZE];
+extern TetrominoType bag[BAG_SIZE] = { 0 };
 
 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
     MODULE (INTERNAL) FUNCTIONS
@@ -56,54 +56,14 @@ void generateNewBag(TetrominoType bag[BAG_SIZE]) {
     HEADER (GLOBAL) FUNCTIONS
 //\\\\\\\\\\\\\\\\\\\\\\\\\//
 
-u16_t viewNextFive() {
-    // grab the next 5 pieces from the facade bag,
-    // and construct the `FivePiecesPreview` union
-    const FivePiecesPreview preview = { .pieces = {
-        .first = facadeBag[0],
-        .second = facadeBag[1],
-        .third = facadeBag[2],
-        .fourth = facadeBag[3],
-        .fifth = facadeBag[4]
-    } };
-
-    // return the corresponding 16-bit integer
-    return preview.integerEncoding;
-}
-
-void printNextFive(const u16_t nextFive) {
-    const FivePiecesPreview preview = { .integerEncoding = nextFive };
-
-    printf("NEXT FIVE PIECES: ");
-    printf("\t1: ");
-    printTetrominoType(preview.pieces.first);
-    printf("\n");
-
-    printf("\t2: ");
-    printTetrominoType(preview.pieces.second);
-    printf("\n");
-
-    printf("\t3: ");
-    printTetrominoType(preview.pieces.third);
-    printf("\n");
-
-    printf("\t4: ");
-    printTetrominoType(preview.pieces.fourth);
-    printf("\n");
-
-    printf("\t5: ");
-    printTetrominoType(preview.pieces.fifth);
-    printf("\n");
-}
-
 TetrominoType drawFromBag() {
-    // if any of the facade bag pointers are `TETROMINO_EMPTY`,
+    // if any of the facade bag pieces are `TETROMINO_EMPTY`,
     // which should only ever happen at the beginning,
-    // or if something went horribly wrong,
-    // regenerate a new bag
+    // (or if something went horribly wrong), then regenerate
+    // a new bag
     for (int k = 0; k < BAG_SIZE; k++) {
-        if (facadeBag[k] == TETROMINO_EMPTY) {
-            generateNewBag(facadeBag); // generate facade bag pieces
+        if (bag[k] == TETROMINO_EMPTY) {
+            generateNewBag(bag); // generate facade bag pieces
 
             actualBag.cursor = 0; // reset the cursor of the actual bag
             generateNewBag(actualBag.pieces); // generate actual bag pieces
@@ -114,9 +74,9 @@ TetrominoType drawFromBag() {
     // the return value is the first facade bag piece;
     // update the remaining pointers EXCEPT for the last one,
     // which will be updated in the next step
-    const TetrominoType returnValue = facadeBag[0];
+    const TetrominoType returnValue = bag[0];
     for (int i = 0; i < BAG_SIZE - 1; i++)
-        facadeBag[i] = facadeBag[i + 1];
+        bag[i] = bag[i + 1];
 
     // if we used up the current bag, then
     // reset the cursor and generate a new bag
@@ -126,7 +86,7 @@ TetrominoType drawFromBag() {
     }
 
     // updated the last piece by generating it from the actual bag
-    facadeBag[BAG_SIZE - 1] = actualBag.pieces[actualBag.cursor++];
+    bag[BAG_SIZE - 1] = actualBag.pieces[actualBag.cursor++];
 
     // return the value
     return returnValue;
